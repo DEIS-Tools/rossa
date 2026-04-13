db6e69ccec4d2784907f2021c153c2a2cfa81f9a
Morten Schou <mksc@cs.aau.dk>
2026-04-13 11:29:04 +0200

    Update experiment configurations and script to match paper

--- demonstration/run_and_generate_plots.sh
+++ demonstration/run_and_generate_plots.sh
@@ -23,16 +23,35 @@ cp $BUILD_PATH/valiant/libvaliant.so .
 # Run demonstration
 DEMONSTRATION_FOLDER=./instances
 
-# PGF=""
-# PGF="--pdf"
-PGF="--pgf"
-FAST=""
-# FAST="--fast"
+# PLOT_FORMAT=""
+PLOT_FORMAT="--pdf"
+# PLOT_FORMAT="--pgf"
+# RUN_FLAGS=""
+RUN_FLAGS="--force --num-workers=4"
 
-python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/example --force $PGF $FAST
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/example $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/example $PLOT_FORMAT
 
-python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_1 --force $PGF $FAST # --num-workers 4 
-python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_1 $PGF
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_1 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_1 $PLOT_FORMAT
 
-python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_2 --force $PGF $FAST # --num-workers 4
-python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_2 $PGF
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_2 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_2 $PLOT_FORMAT
+
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_1_smc_5000 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_1_smc_5000 $PLOT_FORMAT
+
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_1_smc_6000 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_1_smc_6000 $PLOT_FORMAT
+
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_1_smc_7000 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_1_smc_7000 $PLOT_FORMAT
+
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_2_smc_5000 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_2_smc_5000 $PLOT_FORMAT
+
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_2_smc_6000 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_2_smc_6000 $PLOT_FORMAT
+
+python scripts/run_instances.py --directory $DEMONSTRATION_FOLDER/experiment_2_smc_7000 $PLOT_FORMAT $RUN_FLAGS
+python scripts/common_plots_paper.py --output-dir $DEMONSTRATION_FOLDER/experiment_2_smc_7000 $PLOT_FORMAT
